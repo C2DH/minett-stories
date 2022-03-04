e8ce651aab19b05c3c6616fbadf0a86d36ef02c4
gffuma <fumagalli.gf@gmail.com>
2022-03-04 16:45:12 +0100

    Land Of Immortals I wait for my day, base set up for snext ssr

---
+++ src/StaticApp.js
@@ -0,0 +1,46 @@
+import { I18nextProvider } from 'react-i18next'
+import { dehydrate, QueryClient } from 'react-query'
+import { StaticRouter } from 'react-router-dom/server'
+import App from './App'
+import { createI18n } from './i18n'
+
+export default function StaticApp({ queryClient, i18n, url }) {
+  return (
+    <StaticRouter location={url}>
+      <I18nextProvider i18n={i18n}>
+        <App client={queryClient} apiUrl={'https://timestrata.be/api'} />
+      </I18nextProvider>
+    </StaticRouter>
+  )
+}
+
+export const getStaticProps = ({ url }) => {
+  return {
+    props: {
+      i18n: createI18n(url),
+      queryClient: new QueryClient({
+        defaultOptions: {
+          queries: {
+            cacheTime: Infinity,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            refetchInterval: false,
+            refetchIntervalInBackground: false,
+            refetchOnMount: false,
+            staleTime: Infinity,
+            retry: false,
+            suspense: true,
+          },
+        },
+      }),
+    },
+  }
+}
+
+export const getSkeletonProps = (staticProps, { queryClient }) => {
+  return {
+    props: {
+      initialData: dehydrate(queryClient),
+    },
+  }
+}
