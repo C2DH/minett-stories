5ff75c4bb300fa35171969f2d275582138583dcc
skaffo <matteo.scaffidi@gmail.com>
2022-03-08 11:55:39 +0100

    Working on menu page selected

--- src/components/DocItem/DocItem.js
+++ src/components/DocItem/DocItem.js
@@ -17,7 +17,7 @@ function DocItem({ doc }) {
       }}
     >
       <div className={styles.item}>
-        <img width={200} src={imageUrl} alt={doc.data.title} />
+        <img height={100} src={imageUrl} alt={doc.data.title} />
         <h5 className={styles.titleItem}>{doc.data.title}</h5>
       </div>
     </LangLink>
