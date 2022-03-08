3747151fb9e0be1cc06985f5bef4d4a5c5a0ab2f
skaffo <matteo.scaffidi@gmail.com>
2022-03-08 13:11:41 +0100

    Preparing block filters

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -1,29 +1,32 @@
-import Layout from '../components/Layout'
-import pickBy from 'lodash/pickBy'
-import { useSearchParams } from 'react-router-dom'
-import { useDocumentsFacets, useInfiniteDocuments } from '@c2dh/react-miller'
-import { Fragment, useEffect } from 'react'
-import DocItem from '../components/DocItem'
-import { useTranslation } from 'react-i18next'
-import { Waypoint } from 'react-waypoint'
+import Layout from "../components/Layout"
+import pickBy from "lodash/pickBy"
+import { useSearchParams } from "react-router-dom"
+import { useDocumentsFacets, useInfiniteDocuments } from "@c2dh/react-miller"
+import { Fragment, useEffect, useState } from "react"
+import DocItem from "../components/DocItem"
+import { useTranslation } from "react-i18next"
+import { Waypoint } from "react-waypoint"
+import { Offcanvas } from "reactstrap"
+import styles from './Archive.module.css'
 
 export default function Archive() {
   const { t } = useTranslation()
   const [searchParams, setSearchParams] = useSearchParams()
+  const [showFilters, setShowFilters] = useState(false)
   const filters = {
-    data__type: searchParams.get('data__type', '') ?? '',
-    q: searchParams.get('q', '') ?? '',
+    data__type: searchParams.get("data__type", "") ?? "",
+    q: searchParams.get("q", "") ?? "",
   }
 
   useEffect(() => {
     return () => {
-      console.log('Unmount archive')
+      console.log("Unmount archive")
     }
   }, [])
 
   const [docsFacets] = useDocumentsFacets({
     params: {
-      facets: 'data__type',
+      facets: "data__type",
     },
   })
 
@@ -35,12 +38,24 @@ export default function Archive() {
       limit: 50,
       q,
       filters: pickBy(millerFilters, Boolean),
-      exclude: { type: 'entity' },
+      exclude: { type: "entity" },
     },
   })
 
   return (
-    <Layout right={<div>{t('view_options')}</div>}>
+    <Layout
+      right={
+        <div
+          className="cursor-pointer"
+          onClick={() => setShowFilters(!showFilters)}
+        >
+          {showFilters ? t("close_options") : t("view_options")}
+        </div>
+      }
+    >
+      <Offcanvas backdrop={false} direction={'end'} className={styles.filters} isOpen={showFilters}>
+
+      </Offcanvas>
       <div className="container-fluid">
         <div className="row p-5">
           {docGroups &&
