5ab292212bf5655910df4fb1014958338284cc2b
skaffo <matteo.scaffidi@gmail.com>
2022-03-08 10:51:19 +0100

    Working on topbar component

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import { Offcanvas, OffcanvasBody } from 'reactstrap'
-import LangLink from '../LangLink'
-import styles from './TopBar.module.css'
+import { useState } from "react"
+import { Offcanvas, OffcanvasBody } from "reactstrap"
+import ChangeLangLink from "../ChangeLangLink"
+import c2dh from "./assets/c2dh.png"
+import unilu from "./assets/unilu.png"
+import LangLink from "../LangLink"
+import styles from "./TopBar.module.css"
+import { useTranslation } from "react-i18next"
+import { LANGS } from "../../i18n"
 
 export default function TopBar({ right }) {
   const [open, setOpen] = useState(false)
+  const { t } = useTranslation()
+  const { i18n } = useTranslation()
   return (
     <>
       <div className={styles.TopBar}>
@@ -13,7 +20,7 @@ export default function TopBar({ right }) {
             onClick={() => {
               setOpen(!open)
             }}
-            style={{ fontSize: '2rem' }}
+            style={{ fontSize: "2rem" }}
             className="bi bi-list cursor-pointer"
           />
         </div>
@@ -26,10 +33,76 @@ export default function TopBar({ right }) {
         className={styles.Offcanvas}
       >
         <OffcanvasBody className={styles.OffcanvasBody}>
-          <LangLink to="/">Intro</LangLink>
-          <LangLink to="/stories">Stories</LangLink>
-          <LangLink to="/archive">Archive</LangLink>
-          <LangLink to="/about">About</LangLink>
+          <LangLink className={styles.BigLink} to="/">
+            {t("intro")}
+          </LangLink>
+          <LangLink className={styles.BigLink} to="/stories">
+            {t("stories")}
+          </LangLink>
+          <LangLink className={styles.BigLink} to="/archive">
+            {t("archive")}
+          </LangLink>
+          <LangLink className={styles.BigLink} to="/about">
+            {t("about")}
+          </LangLink>
+          <div className={styles.OffcanvasBottom}>
+            <div className={styles.BlockLanguages}>
+              <ChangeLangLink
+                className={
+                  i18n.language === LANGS[0]
+                    ? styles.ChangeLangLinkActive
+                    : styles.ChangeLangLink
+                }
+                lang={LANGS[0]}
+              >
+                EN
+              </ChangeLangLink>
+              <ChangeLangLink
+                className={
+                  i18n.language === LANGS[1]
+                    ? styles.ChangeLangLinkActive
+                    : styles.ChangeLangLink
+                }
+                lang={LANGS[1]}
+              >
+                FR
+              </ChangeLangLink>
+              <ChangeLangLink
+                className={
+                  i18n.language === LANGS[2]
+                    ? styles.ChangeLangLinkActive
+                    : styles.ChangeLangLink
+                }
+                lang={LANGS[2]}
+              >
+                DE
+              </ChangeLangLink>
+            </div>
+            <div className={styles.BlockTerms}>
+              <div>
+                <LangLink className={styles.MediumLink} to="/">
+                  {t("terms_of_use")}
+                </LangLink>
+              </div>
+              <div>
+                <LangLink className={styles.MediumLink} to="/stories">
+                  {t("contact_us")}
+                </LangLink>
+              </div>
+            </div>
+            <div>
+              <img
+                src={c2dh}
+                className={styles.c2dh}
+                alt="Luxembourg Centre for Contemporary and digital History"
+              />
+              <img
+                src={unilu}
+                className={styles.logoUnilu}
+                alt="Universitè du Luxembourg"
+              />
+            </div>
+          </div>
         </OffcanvasBody>
       </Offcanvas>
     </>
