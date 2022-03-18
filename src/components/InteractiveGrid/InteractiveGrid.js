8afbc204d73c0924dbcab35b13f0ffb385e04337
gffuma <fumagalli.gf@gmail.com>
2022-03-18 12:28:29 +0100

    Interactive grid component

---
+++ src/components/InteractiveGrid/InteractiveGrid.js
@@ -0,0 +1,161 @@
+import { useRef, useState } from 'react'
+import { DraggableCore } from 'react-draggable'
+
+const DEFAULT_INITIAL_POSITION = {
+  left: 60,
+  top: 60,
+}
+
+const DEFAULT_POSITION_BOUNDS = {
+  maxLeft: 65,
+  minLeft: 33,
+  maxTop: 72,
+  minTop: 35,
+}
+
+const DEFAULT_HANDLE_RADIUS_PX = 40
+
+export default function InteractiveGrid({
+  video,
+  bottomLeftImageSource,
+  bottomRightImageSource,
+  videoContainerStyle,
+  topLeft,
+  intialPosition = DEFAULT_INITIAL_POSITION,
+  positionBounds = DEFAULT_POSITION_BOUNDS,
+  handleRadiusPx = DEFAULT_HANDLE_RADIUS_PX,
+}) {
+  const containerRef = useRef()
+
+  function handleDrag(_, data) {
+    const cotaniner = containerRef.current
+    const { width, height } = cotaniner.getBoundingClientRect()
+    const left = Math.max(
+      Math.min((data.x * 100) / width, positionBounds.maxLeft),
+      positionBounds.minLeft
+    )
+    const top = Math.max(
+      Math.min((data.y * 100) / height, positionBounds.maxTop),
+      positionBounds.minTop
+    )
+    setPosition({ left, top })
+  }
+
+  const [position, setPosition] = useState(intialPosition)
+
+  return (
+    <div
+      className="w-100 h-100"
+      style={{ position: 'relative' }}
+      ref={containerRef}
+    >
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{
+          zIndex: 1,
+          position: 'absolute',
+          left: '30%',
+          right: 0,
+          top: 0,
+          bottom: '20%',
+          ...videoContainerStyle,
+        }}
+      >
+        {video}
+      </div>
+      <div
+        style={{
+          overflow: 'hidden',
+          zIndex: 2,
+          position: 'absolute',
+          height: `${100 - position.top}%`,
+          width: `${position.left}%`,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            width: '100%',
+            minHeight: '70vh',
+            minWidth: '70vw',
+            height: '100%',
+            backgroundPosition: 'top right',
+            backgroundImage: bottomLeftImageSource
+              ? `url(${bottomLeftImageSource})`
+              : undefined,
+            backgroundColor: bottomLeftImageSource ? undefined : 'var(--brick)',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
+      </div>
+
+      <div
+        style={{
+          backgroundColor: 'var(--black)',
+          zIndex: 3,
+          position: 'absolute',
+          left: 0,
+          width: `${position.left}%`,
+          top: 0,
+          height: `${position.top}%`,
+        }}
+      >
+        {topLeft}
+      </div>
+
+      <div
+        style={{
+          zIndex: 3,
+          overflow: 'hidden',
+          position: 'absolute',
+          width: `${100 - position.left}%`,
+          height: `${100 - position.top}%`,
+          right: 0,
+          bottom: 0,
+        }}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+            width: '100%',
+            minHeight: '70vh',
+            minWidth: '70vw',
+            maxWidth: '100%',
+            height: '100%',
+            backgroundPosition: 'top left',
+            backgroundImage: bottomRightImageSource
+              ? `url(${bottomRightImageSource})`
+              : undefined,
+            backgroundColor: bottomRightImageSource ? undefined : 'var(--green)',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
+      </div>
+
+      <DraggableCore handle=".handle" onDrag={handleDrag}>
+        <div
+          className="handle"
+          style={{
+            backgroundColor: 'white',
+            position: 'absolute',
+            borderRadius: '50%',
+            zIndex: 4,
+            top: `calc(${position.top}% - ${handleRadiusPx / 2}px)`,
+            left: `calc(${position.left}% - ${handleRadiusPx / 2}px)`,
+            right: 0,
+            width: handleRadiusPx,
+            height: handleRadiusPx,
+          }}
+        />
+      </DraggableCore>
+    </div>
+  )
+}
--- src/pages/TestInteractiveVideo.js
+++ src/pages/TestInteractiveVideo.js
@@ -1,134 +1,30 @@
 import { useDocument, useStory } from '@c2dh/react-miller'
-import { DraggableCore } from 'react-draggable'
-import { useState } from 'react'
-
-const HANDLE_R = 40
+import InteractiveGrid from '../components/InteractiveGrid'
 
 export default function TestInteractiveVideo() {
   const [story] = useStory(39)
-  console.log('Story', story)
-
   const video = story.contents.modules[0].object.document
-  // const doc = story.contents.modules[0].objects[0].document
-
   const [doc] = useDocument(2)
-  console.log({ video, doc })
 
-  const [height, setHeight] = useState(30)
-  const [width, setWidth] = useState(60)
-
-  function handleDrag(e, data) {
-    // TODO: Use container ref
-    const WIDTH = window.innerWidth
-    const HEIGHT = window.innerHeight
-
-    const width = (data.x * 100) / WIDTH
-    const height = 100 - (data.y * 100) / HEIGHT
-    // console.log('D', e, data)
-    console.log('W', width, data.x, WIDTH)
-    setWidth(width)
-    setHeight(height)
-  }
+  console.log('Render!')
 
   return (
-    <div className="h-100 w-100" style={{ position: 'relative' }}>
-      <div style={{ zIndex: 999, position: 'fixed', left: 0 }}>
-        <button onClick={() => setHeight((a) => a - 10)}>-</button>
-        <button onClick={() => setHeight((a) => a + 10)}>+</button>
-        {height} H
-        <br />
-        <br />
-        <button onClick={() => setWidth((a) => a - 10)}>-</button>
-        <button onClick={() => setWidth((a) => a + 10)}>+</button>
-        {width} W
-      </div>
-
-      <div
-        className="border d-flex justify-content-center align-items-center"
-        style={{
-          zIndex: 1,
-          position: 'absolute',
-          left: '30%',
-          right: 0,
-          top: 0,
-          bottom: '20%',
-        }}
-      >
+    <InteractiveGrid
+      topLeft={
+        <div className='w-100 h-100 d-flex align-items-center justify-content-center'>
+          Gio Va
+        </div>
+      }
+      video={
         <video
           src={video.url}
           controls
           style={{ height: '100%', width: '100%', objectFit: 'contain' }}
         />
-      </div>
-
-      <div
-        className="border"
-        style={{
-          overflow: 'hidden',
-          zIndex: 2,
-          position: 'absolute',
-          height: `${height}%`,
-          width: `${width}%`,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <div
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            width: '100%',
-            minHeight: '70vh',
-            minWidth: '60vw',
-            height: '100%',
-            backgroundPosition: 'top right',
-            backgroundImage: `url(${doc.attachment})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-          }}
-        />
-      </div>
-
-      <div
-        className="bg-danger"
-        style={{
-          zIndex: 3,
-          position: 'absolute',
-          left: 0,
-          width: `${width}%`,
-          top: 0,
-          height: `${100 - height}%`,
-        }}
-      />
-      <div
-        className="bg-primary"
-        style={{
-          zIndex: 3,
-          position: 'absolute',
-          width: `${100 - width}%`,
-          height: `${height}%`,
-          right: 0,
-          bottom: 0,
-        }}
-      />
-
-      <DraggableCore handle=".handle" onDrag={handleDrag}>
-        <div
-          className="handle"
-          style={{
-            backgroundColor: 'black',
-            position: 'absolute',
-            borderRadius: '50%',
-            zIndex: 99,
-            top: `calc(${100 - height}% - ${HANDLE_R / 2}px)`,
-            left: `calc(${width}% - ${HANDLE_R / 2}px)`,
-            right: 0,
-            width: HANDLE_R,
-            height: HANDLE_R,
-          }}
-        />
-      </DraggableCore>
-    </div>
+      }
+      // bottomLeftImageSource={doc.attachment}
+      // bottomLeftImageSource={null}
+      // bottomRightImageSource={doc.attachment}
+    />
   )
 }
