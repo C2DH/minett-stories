034966bd6062627e90c75680bacba496b334cc68
skaffo <matteo.scaffidi@gmail.com>
2022-04-11 14:38:13 +0200

    Adding style for blackquotes

--- src/components/VisualModule/VisualModule.js
+++ src/components/VisualModule/VisualModule.js
@@ -5,7 +5,6 @@ import ModuleTextGallery from './ModuleTextGallery'
 import ModuleTextObject from './ModuleTextObject'
 
 export default function VisualModule({ millerModule }) {
-  console.log(millerModule)
   switch (millerModule.module) {
     case 'text_object':
       return <ModuleTextObject millerModule={millerModule} />
