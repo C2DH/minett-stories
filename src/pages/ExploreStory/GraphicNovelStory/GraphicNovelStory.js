158ec36f63f4c456301080f33fc084518583e935
skaffo <matteo.scaffidi@gmail.com>
2022-04-15 17:11:52 +0200

    Working on graphic novel

--- src/pages/ExploreStory/GraphicNovelStory/GraphicNovelStory.js
+++ src/pages/ExploreStory/GraphicNovelStory/GraphicNovelStory.js
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { Fragment, useCallback, useMemo, useRef, useState } from 'react'
 import { ArrowDown, ArrowLeft, ArrowRight, SkipForward } from 'react-feather'
 import LangLink from '../../../components/LangLink'
@@ -107,140 +108,155 @@ export default function GraphicNovelStory({ story }) {
   console.log(selectedChapter)
 
   return (
-    <div className="w-100 h-100 d-flex flex-column">
-      <LangLink
-        style={{
-          position: 'absolute',
-          top: 20,
-          left: 20,
-          backgroundColor: 'var(--dark-grey)',
-        }}
-        to={`/story/${story.slug}`}
-        className={'btn-circle cursor-pointer no-link'}
-      >
-        <ArrowLeft />
-      </LangLink>
-      <div className="flex-1" style={{ overflow: 'hidden' }}>
-        <div className="h-100 w-100">
-          {(!animation || !animation.startsWith('end-')) && (
-            <div
-              key={selectedChapter.id}
-              ref={containerRef}
-              className={`h-100 w-100 py-4 d-flex align-items-center ${styles.novel} ${selectedClass}`}
-              style={{ overflowY: 'auto' }}
-              onTransitionEnd={() => {
-                setAnimation((a) => (a ? 'end-' + a.split('-')[1] : null))
-              }}
-            >
-              <GraphicNovelChapter chapter={selectedChapter} />
-            </div>
-          )}
-
-          {enteringChapter && (
-            <div
-              key={enteringChapter.id}
-              className={`h-100 w-100 py-4 d-flex align-items-center ${styles.novel} ${enterClass}`}
-              style={{ overflowY: 'auto' }}
-              onTransitionEnd={() => setAnimation(null)}
-            >
-              <GraphicNovelChapter chapter={enteringChapter} />
-            </div>
-          )}
-        </div>
-      </div>
-      <div className={`${styles.BlockControls} bg-white d-flex w-100`}>
-        <div className="d-flex align-items-center justify-content-center mx-2 mx-md-5">
-          <SkipForward
-            className="cursor-pointer"
-            onClick={() => {
-              const element = containerRef.current
-              if (element) {
-                const { width } = element.getBoundingClientRect()
-                element.scroll({
-                  left: element.scrollLeft + width / 2,
-                  behavior: 'smooth',
-                })
-                console.log({ element })
-              }
-            }}
-            color="black"
-            fill="black"
-          />
-        </div>
-        <div style={{ flex: 1 }} className="d-flex flex-column">
-          <div className="w-100 d-flex" style={{ height: 8 }}>
-            {story.data.chapters.slice(0, -1).map((chapter, i) => (
-              <Fragment key={chapter.id}>
-                {i !== 0 && (
-                  <div className="bg-white h-100" style={{ width: 20 }} />
-                )}
-                <div
-                  onClick={() => setChapterIndex(i)}
-                  className="w-100 h-100 d-flex cursor-pointer"
-                  style={{
-                    backgroundColor: `var(--opacity-color-story-graphic-novel)`,
-                  }}
-                >
-                  <div
-                    style={{
-                      width: chapter.id === selectedChapter.id ? `${(1 * 100).toFixed(4)}%` : 0,
-                      backgroundColor: `var(--color-story-graphic-novel)`,
-                    }}
-                  />
-                </div>
-              </Fragment>
-            ))}
-          </div>
-          <div
-            className="w-100 d-flex align-items-center text-cadet-blue position-relative"
-            style={{ flex: 1 }}
-          >
-            <span className="text-cadet-blue ms-3 d-none d-md-block">
-              {selectedChapter.data.title}
-            </span>
-            {!goDeeper && (
+    <>
+      <div className="w-100 h-100 d-flex flex-column">
+        <LangLink
+          style={{
+            position: 'absolute',
+            top: 20,
+            left: 20,
+            backgroundColor: 'var(--dark-grey)',
+          }}
+          to={`/story/${story.slug}`}
+          className={'btn-circle cursor-pointer no-link'}
+        >
+          <ArrowLeft />
+        </LangLink>
+        <div className="flex-1" style={{ overflow: 'hidden' }}>
+          <div className="h-100 w-100">
+            {(!animation || !animation.startsWith('end-')) && (
               <div
-                onClick={onGoDeeper}
-                style={{ position: 'absolute', left: 0 }}
-                className="w-100 d-flex justify-content-center cursor-pointer"
+                key={selectedChapter.id}
+                ref={containerRef}
+                className={`h-100 w-100 py-4 d-flex align-items-center ${styles.novel} ${selectedClass}`}
+                style={{ overflowY: 'auto' }}
+                onTransitionEnd={() => {
+                  setAnimation((a) => (a ? 'end-' + a.split('-')[1] : null))
+                }}
               >
-                <div
-                  className={`text-color-story-graphic-novel d-flex flex-column align-items-center`}
-                >
-                  <span className="d-none d-md-block">Go deeper (10 min.)</span>
-                  <ArrowDown
-                    className="ms-0"
-                    color={`var(--color-story-graphic-novel)`}
-                  />
-                </div>
+                <GraphicNovelChapter chapter={selectedChapter} />
+              </div>
+            )}
+
+            {enteringChapter && (
+              <div
+                key={enteringChapter.id}
+                className={`h-100 w-100 py-4 d-flex align-items-center ${styles.novel} ${enterClass}`}
+                style={{ overflowY: 'auto' }}
+                onTransitionEnd={() => setAnimation(null)}
+              >
+                <GraphicNovelChapter chapter={enteringChapter} />
               </div>
             )}
           </div>
         </div>
-        <div className="d-flex align-items-center justify-content-center mx-2 mx-md-5">
-          <div
-            className={'btn-circle text-white bg-dark-gray cursor-pointer me-2'}
-            disabled={chapterIndex <= 0}
-            onClick={() => {
-              if (!animation && chapterIndex > 0) {
-                setChapterIndex((i) => i - 1)
-                setAnimation('enter-prev')
-              }
-            }}
-          >
-            <ArrowLeft />
+        <div className={`${styles.BlockControls} bg-white d-flex w-100`}>
+          <div className="d-flex align-items-center justify-content-center mx-2 mx-md-5">
+            <SkipForward
+              className="cursor-pointer"
+              onClick={() => {
+                const element = containerRef.current
+                if (element) {
+                  const { width } = element.getBoundingClientRect()
+                  element.scroll({
+                    left: element.scrollLeft + width / 2,
+                    behavior: 'smooth',
+                  })
+                  console.log({ element })
+                }
+              }}
+              color="black"
+              fill="black"
+            />
           </div>
-          <div
-            className={'btn-circle text-white bg-dark-gray cursor-pointer'}
-            disabled={chapterIndex >= novelChapters.length - 1}
-            onClick={() => {
-              if (!animation && chapterIndex < novelChapters.length - 1) {
-                setChapterIndex((i) => i + 1)
-                setAnimation('enter-next')
-              }
-            }}
-          >
-            <ArrowRight />
+          <div style={{ flex: 1 }} className="d-flex flex-column">
+            <div className="w-100 d-flex" style={{ height: 8 }}>
+              {story.data.chapters.slice(0, -1).map((chapter, i) => (
+                <Fragment key={chapter.id}>
+                  {i !== 0 && (
+                    <div className="bg-white h-100" style={{ width: 20 }} />
+                  )}
+                  <div
+                    onClick={() => setChapterIndex(i)}
+                    className="w-100 h-100 d-flex cursor-pointer"
+                    style={{
+                      backgroundColor: `var(--opacity-color-story-graphic-novel)`,
+                    }}
+                  >
+                    <div
+                      style={{
+                        width:
+                          chapter.id === selectedChapter.id
+                            ? `${(1 * 100).toFixed(4)}%`
+                            : 0,
+                        backgroundColor: `var(--color-story-graphic-novel)`,
+                      }}
+                    />
+                  </div>
+                </Fragment>
+              ))}
+            </div>
+            <div
+              className="w-100 d-flex align-items-center text-cadet-blue position-relative"
+              style={{ flex: 1 }}
+            >
+              <span className="text-cadet-blue ms-3 d-none d-md-block">
+                {selectedChapter.data.title}
+              </span>
+              {!goDeeper && (
+                <div
+                  onClick={onGoDeeper}
+                  style={{ position: 'absolute', left: 0 }}
+                  className="w-100 d-flex justify-content-center cursor-pointer"
+                >
+                  <div
+                    className={`text-color-story-graphic-novel d-flex flex-column align-items-center`}
+                  >
+                    <span className="d-none d-md-block">
+                      Go deeper (10 min.)
+                    </span>
+                    <ArrowDown
+                      className="ms-0"
+                      color={`var(--color-story-graphic-novel)`}
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="d-flex align-items-center justify-content-center mx-2 mx-md-5">
+            <div
+              className={classNames(
+                'btn-circle text-white bg-dark-gray cursor-pointer me-2',
+                {
+                  ControlDisabled: chapterIndex <= 0,
+                }
+              )}
+              onClick={() => {
+                if (!animation && chapterIndex > 0) {
+                  setChapterIndex((i) => i - 1)
+                  setAnimation('enter-prev')
+                }
+              }}
+            >
+              <ArrowLeft />
+            </div>
+            <div
+              className={classNames(
+                'btn-circle text-white bg-dark-gray cursor-pointer',
+                {
+                  ControlDisabled: chapterIndex >= novelChapters.length - 1,
+                }
+              )}
+              onClick={() => {
+                if (!animation && chapterIndex < novelChapters.length - 1) {
+                  setChapterIndex((i) => i + 1)
+                  setAnimation('enter-next')
+                }
+              }}
+            >
+              <ArrowRight />
+            </div>
           </div>
         </div>
       </div>
@@ -251,6 +267,6 @@ export default function GraphicNovelStory({ story }) {
           ))}
         </div>
       )}
-    </div>
+    </>
   )
 }
