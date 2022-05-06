29757e3da7c94713e1623a02eced633222569884
skaffo <matteo.scaffidi@gmail.com>
2022-05-06 12:01:07 +0200

    Fixing module text gallery

--- src/components/VisualModule/ModuleTextGallery.js
+++ src/components/VisualModule/ModuleTextGallery.js
@@ -4,27 +4,37 @@ import ReactMarkdown from 'react-markdown'
 export default function ModuleTextGallery({ millerModule }) {
   if (millerModule.layout === 'gallery-text') {
     return (
-      <div>
-        <Gallery
-          objects={millerModule.gallery.objects}
-          caption={millerModule.gallery.caption}
-        />
-
-        <ReactMarkdown skipHtml={true}>
-          {millerModule.text.content}
-        </ReactMarkdown>
+      <div className="row">
+        <div className='col-md-12'>
+          <Gallery
+            objects={millerModule.gallery.objects}
+            caption={millerModule.gallery.caption}
+          />
+        </div>
+        <div className="text-black offset-md-3 col-md-6 mt-2 mb-2">
+          <ReactMarkdown
+            className="text-object-story text-black"
+            skipHtml={true}
+          >
+            {millerModule.text.content}
+          </ReactMarkdown>
+        </div>
       </div>
     )
   } else {
     return (
-      <div>
-        <ReactMarkdown skipHtml={true}>
-          {millerModule.text.content}
-        </ReactMarkdown>
-        <Gallery
-          objects={millerModule.gallery.objects}
-          caption={millerModule.gallery.caption}
-        />
+      <div className="row">
+        <div className="text-black offset-md-3 col-md-6 mt-2 mb-2">
+          <ReactMarkdown className="text-object-story" skipHtml={true}>
+            {millerModule.text.content}
+          </ReactMarkdown>
+        </div>
+        <div className="col-md-12">
+          <Gallery
+            objects={millerModule.gallery.objects}
+            caption={millerModule.gallery.caption}
+          />
+        </div>
       </div>
     )
   }
