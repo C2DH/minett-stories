2dbc04b74fa21210b150bac7b1d847748adfc166
skaffo <matteo.scaffidi@gmail.com>
2022-05-09 14:51:32 +0200

    Fixing terms of use

--- src/pages/TermsOfUse/TermsOfUse.js
+++ src/pages/TermsOfUse/TermsOfUse.js
@@ -3,15 +3,15 @@ import ReactMarkdown from 'react-markdown'
 import Layout from '../../components/Layout'
 import styles from './TermsOfUse.module.css'
 
-export default function About() {
+export default function TermsOfUse() {
   const [story] = useStory('terms-of-use')
   return (
     <Layout>
-      <div className="container-fluid padding-top-bar">
-        <div className="row">
-          <div className="col-md-8">
-            <h1>{story.data.title}</h1>
-            <div className={styles.TextAbout}>
+      <div className={`padding-top-bar`}>
+      <h1 className={`${styles.Title}`}>{story.data.title}</h1>
+        <div className={`${styles.TermsOfUse}`}>
+          <div className={`${styles.TermsOfUseInfo}`}>           
+            <div className={styles.TextTermsOfUse}>
               <ReactMarkdown linkTarget="_blank">
                 {story.data.abstract}
               </ReactMarkdown>
