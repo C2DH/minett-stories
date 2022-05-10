2da852f412af29b47495da0328aed400ee78ee2d
skaffo <matteo.scaffidi@gmail.com>
2022-05-10 11:24:44 +0200

    Fixing cursor logo home #89 and fix map story #91

--- src/pages/ExploreStory/MapStory/MapStory.js
+++ src/pages/ExploreStory/MapStory/MapStory.js
@@ -186,7 +186,7 @@ export default function MapStory({ story }) {
       </div>
       {goDeeper && (
         <div className={styles.Content}>
-          <div className="row pt-4 text-black">
+          <div className="bg-white row pt-4 text-black">
             <div className="col-md-6 offset-md-3 d-flex flex-column align-items-start">
               <StoryPill type={type} />
               <h1 className={`${styles.TitleStory} m-0 p-0 mt-3`}>
