5f97ccb7748f68f23f7947325728c520ca5c85fe
Daniele Guido <gui.daniele@gmail.com>
2022-05-27 09:57:01 +0200

    add link to cookie consent

--- src/components/CookieConsent.js
+++ src/components/CookieConsent.js
@@ -1,36 +1,39 @@
-import { useEffect, useState } from 'react'
+import { useState, useLayoutEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Modal, ModalBody } from 'reactstrap'
+import { useLocalStorage } from '../hooks/localStorage'
+import LangLink from './LangLink'
 
 export default function CookieConsent() {
+  const [acceptCookies, setAcceptCookies ] = useLocalStorage('cookieconsent', false)
   const [isOpen, setIsOpen] = useState(false)
-  const storeKey = 'cookieconsent'
 
-  useEffect(() => {
-    const showed = sessionStorage.getItem(storeKey)
-    if (!showed) {
+  useLayoutEffect(() => {
+    if (!acceptCookies) {
       setIsOpen(true)
     }
-  }, [storeKey])
+  }, [acceptCookies])
 
   const { t } = useTranslation()
 
   return (
     <Modal backdrop={false} isOpen={isOpen} className={"cookie-consent-modal"}>
-      <ModalBody className="auto-tip-modal bg-site-black rounded">
+      <ModalBody className="auto-tip-modal bg-site-black rounded shadow">
         <div className="text-center">
-            {t('text_cookie_consent')}
-        </div>
-        <div className="text-center">
+          {t('text_cookie_consent')} <LangLink
+            to={`/terms-of-use`}
+            className="text-white"
+          >
             {t('read_more')}
+          </LangLink>
         </div>
 
         <div className="text-center mt-2">
           <button
-            className="btn-auto-tip btn text-white rounded-pill bg-dark-gray"
+            className="btn-auto-tip btn text-white rounded-pill bg-dark-gray shadow border"
             onClick={() => {
-              setIsOpen(false)
-              sessionStorage.setItem(storeKey, 'yes')
+              setIsOpen(false);
+              setAcceptCookies('yes')
             }}
           >
             {t('ok_i_got')}
