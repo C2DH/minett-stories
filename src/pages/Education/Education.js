45ffc6327e311321f6836d81adff02fb671bcdd7
Daniele Guido <gui.daniele@gmail.com>
2023-06-26 10:29:54 +0200

    add education Vimeo iframe

--- src/pages/Education/Education.js
+++ src/pages/Education/Education.js
@@ -4,6 +4,8 @@ import ReactMarkdown from 'react-markdown'
 import Layout from '../../components/Layout'
 import styles from './Education.module.css'
 import socialMediaImage from '../../assets/socialMediaImage.png'
+import vimeo from 'react-player'
+import VimeoPlayer from 'react-player/vimeo'
 
 export default function Education() {
   const [story] = useStory('education')
@@ -18,7 +20,74 @@ export default function Education() {
         <div className={`${styles.Education}`}>
           <div className={`${styles.EducationInfo}`}>
             <div className={styles.TextEducation}>
-              <ReactMarkdown linkTarget="_blank">
+              <ReactMarkdown
+                linkTarget="_blank"
+                components={{
+                  p({ node, inline, className, children, ...props }) {
+                    // if one of the chimldren is of tagName 'a' and has href containing vimeo.com
+                    if (
+                      children.length === 1 &&
+                      typeof children[0] === 'object'
+                    ) {
+                      const href = children[0].props?.href
+                      if (href && href.indexOf('https://vimeo.com') !== -1) {
+                        return (
+                          <div
+                            style={{
+                              padding: '56.25% 0 0 0',
+                              position: 'relative',
+                              backgroundColor: 'var(--brick)',
+                            }}
+                            className="position-relative mb-3"
+                          >
+                            <div className="position-absolute w-100 h-100 top-0">
+                              {children}
+                            </div>
+                          </div>
+                        )
+                      }
+                    }
+
+                    //   return (
+                    //     <VimeoPlayer
+                    //       playsinline
+                    //       playing={false}
+                    //       controls
+                    //       url={props.href}
+                    //     />
+                    //   )
+                    // }
+
+                    return (
+                      <p {...props} className={className}>
+                        {children}
+                      </p>
+                    )
+                  },
+
+                  a({ node, inline, className, children, ...props }) {
+                    // if href co,ntains vimeo.com
+                    if (props.href.indexOf('https://vimeo.com') !== -1) {
+                      return (
+                        <VimeoPlayer
+                          playsinline
+                          playing={false}
+                          controls
+                          url={props.href}
+                          width={'100%'}
+                          height={'100%'}
+                        />
+                      )
+                    }
+
+                    return (
+                      <a {...props} className={className}>
+                        {children}
+                      </a>
+                    )
+                  },
+                }}
+              >
                 {story.data.abstract}
               </ReactMarkdown>
             </div>
